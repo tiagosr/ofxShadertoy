57135d4d32c54e2be816b99e84d2380cbba0d955
Tiago Rezende <tiagosr@gmail.com>
2017-08-27 00:35:44 +0300

    Changed samples init code to be compatible with oF 0.9.x

--- raymarcherExample/src/main.cpp
+++ raymarcherExample/src/main.cpp
@@ -3,7 +3,12 @@
 
 //========================================================================
 int main( ){
-    ofSetCurrentRenderer(ofGLProgrammableRenderer::TYPE);
-	ofSetupOpenGL(1024,768,OF_WINDOW);
+    ofGLWindowSettings settings;
+    settings.setGLVersion(3, 2);    // <--- ofxShadertoy NEEDS the GL Programmable Renderer
+    settings.width = 1024;
+    settings.height = 768;
+    ofCreateWindow(settings);       // <-------- setup the GL context
+    
+    // this kicks off the running of my app
     ofRunApp(new ofApp());
 }
